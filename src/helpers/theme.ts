a6f7137a0a2993f1f616f662f7f63f9bb5333a6f
Ramazan Izbagov <izbagov@gmail.com>
2023-05-27 22:47:03 +0300

    Add header

---
+++ src/helpers/theme.ts
@@ -0,0 +1,20 @@
+export const getTheme = () => {
+  if (typeof localStorage !== 'undefined' && localStorage.getItem('theme')) {
+    return localStorage.getItem('theme') as string;
+  }
+  if (window.matchMedia('(prefers-color-scheme: dark)').matches) {
+    return 'dark';
+  }
+  return 'light';
+};
+
+export const setThemeColor = (isLight: boolean) => {
+  const root = document.documentElement;
+  if (isLight) {
+    window.localStorage.setItem('theme', 'light');
+    root.classList.remove('theme-dark');
+  } else {
+    window.localStorage.setItem('theme', 'dark');
+    root.classList.add('theme-dark');
+  }
+};
