ced7e85d3fc7261affa88f23c43d06d1971fa726
Ramazan Izbagov <izbagov@gmail.com>
2024-03-17 03:16:17 +0300

    update astro-icon package and add responsive for lessons block and footer

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,12 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import react from '@astrojs/react';
-
 import mdx from '@astrojs/mdx';
+
+import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), react(), mdx()],
-  prefetch: true,
+  integrations: [tailwind(), react(), mdx(), icon()],
+  prefetch: true
 });
